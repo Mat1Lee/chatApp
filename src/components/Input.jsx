0fdb26efada540b7ad268b79bd53fe06955ee6b0
Matt Le <manhnghean183286@gmail.com>
2024-07-03 10:42:34 +0700

    feat

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,121 +2,118 @@ import React, { useContext, useState } from "react";
 
 import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye,faPaperPlane,faPaperclip,faFaceSmile,faFileImage,  } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEye,
+  faPaperPlane,
+  faPaperclip,
+  faFaceSmile,
+  faFileImage,
+} from "@fortawesome/free-solid-svg-icons";
 import {
   arrayUnion,
   doc,
   serverTimestamp,
   setDoc,
   Timestamp,
-  updateDoc,onSnapshot
+  updateDoc,
+  onSnapshot,
 } from "firebase/firestore";
 import { db, storage } from "../firebase";
 import { v4 as uuid } from "uuid";
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import formatRelative from "date-fns/esm/fp/formatRelative/index.js";
-import {formatDate} from "../helpers/index.js";
+import { formatDate } from "../helpers/index.js";
 const Input = () => {
   const [text, setText] = useState("");
   const [img, setImg] = useState(null);
-  const [doc1,setDoc1] = useState('');
+  const [doc1, setDoc1] = useState("");
   const { currentUser } = useContext(AuthContext);
   const { data } = useContext(ChatContext);
   // console.log(data)
 
-const getChats = () => {
-  const unsub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
-    setDoc1(doc._document)
-  });
-
-  return () => {
-    unsub();
-  };
-}
-
- getChats();
-  const handleSend = async () => {
-    if(text!== ''){
- if (img) {
-      const storageRef = ref(storage, uuid());
-      
-      const uploadTask = uploadBytesResumable(storageRef, img);
-        // console.log(data.chatId);
-      uploadTask.on(
-        (error) => {
-         
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-            await updateDoc(doc(db, "chats", data?.chatId), {
-              messages: arrayUnion({
-                id: uuid(),
-                text,
-                senderId: currentUser.uid,
-                date: formatDate(Timestamp.now()),
-                img: downloadURL,
-              }),
-            });
-          });
-        }
-      );
-    } else {
-
-      if(doc1 !== null){
-   await updateDoc(doc(db, "chats", data?.chatId), {
-        messages: arrayUnion({
-          id: uuid(),
-          text:text.trim(),
-          senderId: currentUser.uid,
-          date: formatDate(Timestamp.now()),
-          photoURL:currentUser.photoURL,
-        
-        }),
-      });
-
-
-      }
-       else {
-        await setDoc(doc(db, "chats", data?.chatId), {
-          messages: arrayUnion({
-            id: uuid(),
-            text,
-            senderId: currentUser.uid,
-            date: formatDate(Timestamp.now()),
-            photoURL:currentUser.photoURL,
-          
-          }),
-        });
-       }
-   
-    }
-
-    await updateDoc(doc(db, "userChats", currentUser.uid), {
-      [data?.chatId + ".lastMessage"]: {
-        text,
-      },
-      [data?.chatId + ".date"]: serverTimestamp(),
+  const getChats = () => {
+    const unsub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+      setDoc1(doc._document);
     });
 
-   
-    setText("");
-    setImg(null);
-
-    } else alert('nhap tin nhan')
-   
+    return () => {
+      unsub();
+    };
   };
-  
+
+  getChats();
+  const handleSend = async () => {
+    if (text !== "") {
+      if (img) {
+        const storageRef = ref(storage, uuid());
+
+        const uploadTask = uploadBytesResumable(storageRef, img);
+        // console.log(data.chatId);
+        uploadTask.on(
+          (error) => {},
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then(
+              async (downloadURL) => {
+                await updateDoc(doc(db, "chats", data?.chatId), {
+                  messages: arrayUnion({
+                    id: uuid(),
+                    text,
+                    senderId: currentUser.uid,
+                    date: formatDate(Timestamp.now()),
+                    img: downloadURL,
+                  }),
+                });
+              }
+            );
+          }
+        );
+      } else {
+        if (doc1 !== null) {
+          await updateDoc(doc(db, "chats", data?.chatId), {
+            messages: arrayUnion({
+              id: uuid(),
+              text: text.trim(),
+              senderId: currentUser.uid,
+              date: formatDate(Timestamp.now()),
+              photoURL: currentUser.photoURL,
+            }),
+          });
+        } else {
+          await setDoc(doc(db, "chats", data?.chatId), {
+            messages: arrayUnion({
+              id: uuid(),
+              text,
+              senderId: currentUser.uid,
+              date: formatDate(Timestamp.now()),
+              photoURL: currentUser.photoURL,
+            }),
+          });
+        }
+      }
+
+      await updateDoc(doc(db, "userChats", currentUser.uid), {
+        [data?.chatId + ".lastMessage"]: {
+          text,
+        },
+        [data?.chatId + ".date"]: serverTimestamp(),
+      });
+
+      setText("");
+      setImg(null);
+    } else alert("nhap tin nhan");
+  };
+
   return (
     <div className="input">
-      <input
+      <Input
         type="text"
         placeholder="Type something..."
         onChange={(e) => setText(e.target?.value)}
         value={text}
       />
       <div className="send">
-      <FontAwesomeIcon  className="icon-send" icon={faPaperclip}/>
+        <FontAwesomeIcon className="icon-send" icon={faPaperclip} />
         <input
           type="file"
           style={{ display: "none" }}
@@ -124,12 +121,14 @@ const getChats = () => {
           onChange={(e) => setImg(e.target.files[0])}
         />
         <label htmlFor="file">
-          <FontAwesomeIcon className="icon-send" icon={faFileImage}/>
+          <FontAwesomeIcon className="icon-send" icon={faFileImage} />
         </label>
         {/* <label htmlFor="file">
           <img src={Img} alt="" />
         </label> */}
-        <button onClick={handleSend}><FontAwesomeIcon className="icon-send" icon={faPaperPlane}/></button>
+        <button onClick={handleSend}>
+          <FontAwesomeIcon className="icon-send" icon={faPaperPlane} />
+        </button>
       </div>
     </div>
   );
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import {
   collection,
   query,
@@ -6,19 +6,21 @@ import {
   getDocs,
   setDoc,
   doc,
+  getDoc,
   updateDoc,
   serverTimestamp,
-  getDoc,
 } from "firebase/firestore";
-
-
+import { Input } from "antd";
 import { db } from "../firebase";
 import { AuthContext } from "../context/AuthContext";
+import { debounce } from "lodash";
+import { ChatContext } from "../context/ChatContext";
+
 const Search = () => {
   const [username, setUsername] = useState("");
   const [user, setUser] = useState(null);
   const [err, setErr] = useState(false);
-
+  const { Search } = Input;
   const { currentUser } = useContext(AuthContext);
 
   const handleSearch = async () => {
@@ -32,7 +34,7 @@ const Search = () => {
     );
 
     try {
-      const querySnapshot = await getDocs(q); 
+      const querySnapshot = await getDocs(q);
       if (querySnapshot.empty) {
         setErr(true);
         setUser(null);
@@ -43,72 +45,72 @@ const Search = () => {
           userData.push(doc.data());
         });
         setUser(userData.length > 0 ? userData[0] : null);
+        console.log(user, 'asdasd');
       }
     } catch (err) {
       console.log(err);
       setErr(true);
     }
   };
-  const handleKey = (e) => {
-    e.code === "Enter" && handleSearch();
-  };
+const debounceSearch = debounce(handleSearch, 500);
+  useEffect(() => {
+    debounceSearch()
+  }, [username]);
+  // const handleSelect = async () => {
+  //   //check whether the group(chats in firestore) exists, if not create
+  //   const combinedId =
+  //     currentUser.uid > user.uid
+  //       ? currentUser.uid + user.uid
+  //       : user.uid + currentUser.uid;
 
-  const handleSelect = async () => {
-    //check whether the group(chats in firestore) exists, if not create
-    const combinedId =
-      currentUser.uid > user.uid
-        ? currentUser.uid + user.uid
-        : user.uid + currentUser.uid;
+  //       console.log(combinedId);
+  //       console.log('user',user)
+  //   try {
+  //     const res = await getDoc(doc(db, "chats", combinedId));
 
-        console.log(combinedId);
-        console.log('user',user)
-    try {
-      const res = await getDoc(doc(db, "chats", combinedId));
+  //     if (!res.exists()) {
+  //       //create a chat in chats collection
+  //       await setDoc(doc(db, "chats", combinedId), { messages: [] });
 
-      if (!res.exists()) {
-        //create a chat in chats collection
-        await setDoc(doc(db, "chats", combinedId), { messages: [] });
+  //       //create user chats
+  //       await updateDoc(doc(db, "userChats", currentUser.uid), {
+  //         [combinedId + ".userInfo"]: {
+  //           uid: user.uid,
+  //           displayName: user.displayName,
+  //           photoURL: user.photoURL,
+  //         },
+  //         [combinedId + ".date"]: serverTimestamp(),
+  //       });
 
-        //create user chats
-        // await updateDoc(doc(db, "userChats", currentUser.uid), {
-        //   [combinedId + ".userInfo"]: {
-        //     uid: user.uid,
-        //     displayName: user.displayName,
-        //     photoURL: user.photoURL,
-        //   },
-        //   [combinedId + ".date"]: serverTimestamp(),
-        // });
+  //       await updateDoc(doc(db, "userChats", user.uid), {
+  //         [combinedId + ".userInfo"]: {
+  //           uid: currentUser.uid,
+  //           displayName: currentUser.displayName,
+  //           photoURL: currentUser.photoURL,
+  //         },
+  //         [combinedId + ".date"]: serverTimestamp(),
+  //       });
+  //     }
+  //   } catch (err) {}
 
-        // await updateDoc(doc(db, "userChats", user.uid), {
-        //   [combinedId + ".userInfo"]: {
-        //     uid: currentUser.uid,
-        //     displayName: currentUser.displayName,
-        //     photoURL: currentUser.photoURL,
-        //   },
-        //   [combinedId + ".date"]: serverTimestamp(),
-        // });
-      }
-    } catch (err) {}
-
-    setUser(null);
-    setUsername("")
+  //   setUser(null);
+  //   setUsername("")
+  // };
+  const { dispatch, data } = useContext(ChatContext);
+  const handleSelect = (u) => {
+    console.log(u);
+    dispatch({ type: "CHANGE_USER", payload: u });
   };
   return (
     <div className="search">
-  
-
-      <div className="searchForm">
-        <input
-          type="text"
-          placeholder="Find a user"
-          onKeyDown={handleKey}
-          onChange={(e) => setUsername(e.target.value)}
-          value={username}
-        />
-      </div>
+      <Search
+        placeholder="Nhập tên người dùng"
+        onChange={(e) => setUsername(e.target.value)}
+        allowClear
+      />
       {err && <span>User not found!</span>}
-      {user && (
-        <div className="userChat" onClick={handleSelect}>
+      {user &&username && (
+        <div className="userChat" onClick={()=>handleSelect(user)}>
           <img src={user.photoURL} alt="" />
           <div className="userChatInfo">
             <span>{user.displayName}</span>
