cfe442fc9ed2c584fe6ab853b36b57ebcc8a99d1
Matt Le <manhnghean183286@gmail.com>
2024-07-04 13:25:20 +0700

    feat

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development', // hoặc 'production'
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i, // Áp dụng cho cả .sass và .scss
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    static: path.join(__dirname, 'dist'), // cập nhật từ contentBase thành static trong Webpack 5
+    compress: true,
+    port: 9000,
+  },
+};
